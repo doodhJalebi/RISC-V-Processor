ec466cd2f016e267fd1a8fc7f964bfd092827bce
Ali Hamza <ah05084@st.habib.edu.pk>
2020-06-26 01:48:29 +0500

    TB changed

--- tbRISCV.v
+++ tbRISCV.v
@@ -1,22 +1,29 @@
-module tbRISCV();
+module tb
+(
+);
 
-    reg clk;
-    reg reset;
+reg clk;
+reg reset;
 
-    RISC_V_Processor RP
-    (
-    .clk(clk),
-    .reset(reset)
-    );
+RISC_V_Processor r1
+(
+  .clk(clk),
+  .reset(reset)
+);
 
-    initial
+initial
+begin
+  clk <= 1'b0;
+  reset <= 1'b0;
+end
 
-    begin
-    clk = 1'b0;
-    reset = 1'b0;
-    end
+always
+#5 clk = ~clk;
 
-    always
-    #10 clk = ~clk;
+initial
+begin
+#35 reset <= 1'b1;
+#1	reset <= 1'b0;
+end
 
 endmodule
